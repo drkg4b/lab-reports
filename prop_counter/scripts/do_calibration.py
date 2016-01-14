30e6fbcc260cf7b6411db2f85f438b9fb0086aba
Gabriele Bertoli <gbertoli@cern.ch>
2016-01-14 16:07:25 +0100

    do_calibration.py: Added plot for the calibration

--- prop_counter/scripts/do_calibration.py
+++ prop_counter/scripts/do_calibration.py
@@ -4,8 +4,8 @@
 Script to read the mca calibration file and calculate the calibration constant.
 '''
 
-from ROOT import TH1F
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 
 
@@ -47,14 +47,33 @@ def main():
     Function to start the program.
     '''
 
-    file_name = sys.argv[1]
+    # file_name = sys.argv[1]
 
-    y_range = read_file(file_name)
-    x_range = [i for i in xrange(len(y_range))]
+    # y_range = read_file(file_name)
+    # x_range = [i for i in xrange(len(y_range))]
 
-    plt.step(x_range, y_range, where='mid')
-    plt.show()
+    # plt.step(x_range, y_range, where='mid')
+    # plt.show()
 
+    x_centroids = [66.79, 134.06, 198.92, 267.73, 333.49, 393.93]
+    y_charge = [2.5, 5, 7.5, 10, 12.5, 15]
+
+    # plt.errorbar(x_centroids, y_charge, xerr=0, yerr=1)
+    plt.xlabel("MCA #")
+    plt.ylabel("Charge [pC]")
+
+    fit_coeff = np.polyfit(x_centroids, y_charge, 1)
+
+    z = np.poly1d(fit_coeff)
+
+    plt.plot(x_centroids, y_charge, 'bs', label='Data points')
+    plt.plot(x_centroids, z(x_centroids), 'g', label='Polinomial fit')
+
+    plt.legend(loc=2)
+
+    plt.grid(True)
+
+    plt.savefig('calibration.pdf')
 
 if __name__ == '__main__':
 
